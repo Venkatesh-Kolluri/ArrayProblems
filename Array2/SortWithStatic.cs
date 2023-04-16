5cd36669a76cefe27fdb46bf5032700fe822dab1
venky <venky94.vk@gmail.com>
2023-04-16 13:18:16 +0530

    [Array Added Successfully]

---
+++ Array2/SortWithStatic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array2
+{
+    internal class SortWithStatic
+    {
+        public static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Enter the size of an array: ");
+            size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the elements into an array: ");
+            int[] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+
+                    if (arr[i] < arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+
+                    }
+                }
+
+            }
+            Console.Write("The descending order of  an array is: ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+            Console.WriteLine(" ");
+
+
+
+        }
+    }
+}
